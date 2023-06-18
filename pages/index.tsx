10f1c1162a4d4f16d1628d2fbadf0a54e8fee1d7
Henrique Silva <henrique@henriquesilva.dev>
2023-06-18 12:00:18 -0300

    add name in input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { GlobalStyles } from "@ui/theme/GlobalStyles";
 import { todoController } from "@ui/controller/todo";
-import { todo } from "node:test";
-import { set } from "zod";
 
 const bg = "./bg.jpeg";
 
@@ -73,6 +71,7 @@ function HomePage() {
           }}
         >
           <input
+            name="add-todo"
             type="text"
             placeholder="Correr, Estudar..."
             value={newTodoContent}
